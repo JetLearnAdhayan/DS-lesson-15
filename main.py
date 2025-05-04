8ba309b54224d3c86d855a772e6974c0948c0c05
JetLearnAdhayan <adhayanraj9@gmail.com>
2025-05-04 12:54:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+
+
+data = pd.read_csv("Covid.csv")
+
+data.columns = ("Date_reported", "Country_Code", "Country", "WHO_region","New_cases","Cumulative_cases", "New_deaths", "Cumulative_deaths")
+
+data["Date_reported"] = pd.to_datetime(data["Date_reported"])
+
+data_dates = data.groupby("Date_reported").sum()
+
+fig1 = go.Figure()
+fig1.add_trace(go.Scatter(x = data_dates.index, y = data_dates["Cumulative_cases"],fill = 'tonexty', line_color = "blue"))
+
+fig1.update_layout(title = "Cumulative Cases Worldwide")
+fig1.write_html("Fig1.html", auto_open=True)
+
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x = data_dates.index, y = data_dates["New_cases"],fill="tonexty", line_color = "blue"))
+fig2.update_layout(title = "New Cases Worldwide")
+
+fig2.write_html("Fig2.html", auto_open=True)
