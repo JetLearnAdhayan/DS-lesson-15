8249328d2369bd6b49c9ef64354d08839d2d9cef
JetLearnAdhayan <adhayanraj9@gmail.com>
2025-05-11 12:30:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,12 +13,20 @@ data_dates = data.groupby("Date_reported").sum()
 
 fig1 = go.Figure()
 fig1.add_trace(go.Scatter(x = data_dates.index, y = data_dates["Cumulative_cases"],fill = 'tonexty', line_color = "blue"))
-
 fig1.update_layout(title = "Cumulative Cases Worldwide")
 fig1.write_html("Fig1.html", auto_open=True)
 
 fig2 = go.Figure()
 fig2.add_trace(go.Scatter(x = data_dates.index, y = data_dates["New_cases"],fill="tonexty", line_color = "blue"))
 fig2.update_layout(title = "New Cases Worldwide")
-
 fig2.write_html("Fig2.html", auto_open=True)
+
+fig3 = go.Figure()
+fig3.add_trace(go.Scatter(x = data_dates.index, y= data_dates["New_deaths"],fill="tonexty", line_color = "blue"))
+fig3.update_layout(title = "New Deaths Worldwide")
+fig3.write_html("Fig3.html", auto_open =True)
+
+fig4 = go.Figure()
+fig4.add_trace(go.Scatter(x = data_dates.index, y=data_dates["Cumulative_deaths"],fill = "tonexty", line_color = "red"))
+fig4.update_layout(title = "Cumulative Deaths Worldwide")
+fig4.write_html("Fig4.html", auto_open =True)
